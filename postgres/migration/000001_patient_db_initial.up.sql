84d7a08a72b3a017a8e1ab51dbe2922f565d88bb
lxiong1 <luexionglx@gmail.com>
2024-01-08 10:59:00 -0600

    Add migrations script for patient

---
+++ postgres/migration/000001_patient_db_initial.up.sql
@@ -0,0 +1,19 @@
+CREATE TYPE "enum_gender" AS ENUM (
+    'M',
+    'F',
+    'U'
+);
+
+CREATE TABLE "patient" (
+    "id" UUID PRIMARY KEY,
+    "member_id" INT UNIQUE NOT NULL,
+    "client_id" VARCHAR(255),
+    "first_name" VARCHAR(64) NOT NULL,
+    "last_name" VARCHAR(64) NOT NULL,
+    "gender" enum_gender NOT NULL DEFAULT 'U',
+    "date_of_birth" DATE NOT NULL,
+    "phone_number" VARCHAR(15),
+    "email" VARCHAR(64),
+    "date_created" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "date_modified" TIMESTAMPTZ NOT NULL DEFAULT now()
+);
