059e5693bf26c720199ed0d819a0890817e74ebe
lxiong1 <luexionglx@gmail.com>
2024-01-22 11:22:45 -0600

    Add omitempties to models

--- api/model/address.go
+++ api/model/address.go
@@ -4,13 +4,13 @@ import "time"
 
 type Address struct {
 	Id             string     `json:"id" db:"id"`
-	City           string     `json:"city" db:"city"`
-	AddressLineOne string     `json:"addressLineOne" db:"address_line_one"`
-	AddressLineTwo *string    `json:"addressLineTwo" db:"address_line_two"`
-	State          string     `json:"state" db:"state"`
-	Zip            string     `json:"zip" db:"zip"`
-	Country        string     `json:"country" db:"country"`
-	DateCreated    *time.Time `json:"dateCreated" db:"date_created"`
-	DateModified   *time.Time `json:"dateModified" db:"date_modified"`
+	City           string     `json:"city,omitempty" db:"city"`
+	AddressLineOne string     `json:"addressLineOne,omitempty" db:"address_line_one"`
+	AddressLineTwo *string    `json:"addressLineTwo,omitempty" db:"address_line_two"`
+	State          string     `json:"state,omitempty" db:"state"`
+	Zip            string     `json:"zip,omitempty" db:"zip"`
+	Country        string     `json:"country,omitempty" db:"country"`
+	DateCreated    *time.Time `json:"dateCreated,omitempty" db:"date_created"`
+	DateModified   *time.Time `json:"dateModified,omitempty" db:"date_modified"`
 	PatientId      string     `json:"patientId" db:"patient_id"`
 }
